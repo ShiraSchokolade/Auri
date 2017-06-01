1632de865812eff168e4589a38506acfe2198270
Sarah <daenearys@gmx.de>
2017-06-01 16:51:44 +0200

    main menu + level 1 scene with background and foreground

---
+++ Assets/Scripts/MainMenuManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuManager : MonoBehaviour {
+
+	void Start () {
+		
+	}
+	
+	void Update () {
+		
+	}
+
+  public void LoadScene()
+  {
+    SceneManager.LoadScene("Prototype");
+  }
+}
